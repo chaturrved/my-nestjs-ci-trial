ccae6fd4c623a8f12a97fc4581a63c9c104c196c
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-07-13 06:39:07 +0530

    final test for ci-cd

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World from Nest Service!');
+      expect(appController.getHello()).toBe('Hello World final!');
     });
   });
 });
